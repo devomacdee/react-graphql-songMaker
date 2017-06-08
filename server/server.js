2e154be50c9f1d8b43526e15ae99602c25a4dbef
Devin McCoy <devingmccoy@gmail.com>
2017-06-08 11:06:52 -0400

    started adding this to git a little late!

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://devin:devin@ds111622.mlab.com:11622/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
